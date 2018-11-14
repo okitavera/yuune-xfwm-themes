00270d4a196f76530923a36ef813d77b41fda0e6
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-11-14 13:15:31 +0000

    compress svg with python-scour

---
+++ Avrora/Avrora-Ein/xfwm4/compress.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+pushd $(dirname $(readlink -f $0))/assets
+rm -rf scour
+mkdir scour
+for i in *.svg
+do
+  scour -i $i -o scour/$i \
+    --strip-xml-prolog \
+    --enable-viewboxing \
+    --enable-id-stripping \
+    --enable-comment-stripping \
+    --shorten-ids \
+    --indent=space --nindent=3
+done
+cp scour/*.svg .
+rm -rf scour
+popd
